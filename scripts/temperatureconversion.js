fe99b41267c61c7cf80616be988882393d8549a3
Tamarzi <simonalale@gmail.com>
2022-03-31 11:01:19 +0100

    removal of folder causing rotten branch

--- scripts/temperatureconversion.js
+++
@@ -1,9 +0,0 @@
-const celsiusToFahrenheit = (celsius) => {
-    return (9 * celsius)/5 + 32;
-}
-
-const fahrenheitToCelsius = (fahrenheit) => {
-    return (fahrenheit - 32) * (5/9);
-}
-
-export {celsiusToFahrenheit, fahrenheitToCelsius};
