082f9ab32e3ea0fd65498d96f3d894ba223b6df1
Tamarzi <simonalale@gmail.com>
2022-04-01 16:36:47 +0100

    feature(openweatherqueries): Minor corrections changed the object name from keys to linkedMetaData, added unit initialized to metric, added the unit parameter to the URL in the both functions

--- scripts/weatherqueries.js
+++ scripts/weatherqueries.js
@@ -1,9 +1,10 @@
-const keys = {
-    openWeatherKey: 'b2d92d739223cff08ee593caea6203b6'
+const linkMetaData = {
+    openWeatherKey: 'b2d92d739223cff08ee593caea6203b6',
+    unit: 'metric',
 }
 
 const weatherQueriesWithCoord = async(coordinates) => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${keys.openWeatherKey}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=${linkMetaData.unit}&appid=${linkMetaData.openWeatherKey}`;
 
     const response = await fetch(URL, {
         "Method": "GET"
@@ -15,14 +16,14 @@ const weatherQueriesWithCoord = async(coordinates) => {
     const wind = weatherData.wind.speed;
     const humidity = weatherData.main.humidity;
     const pressure = weatherData.main.pressure;
-    const iconId = weatherData.main.icon;
-    const iconName = weatherData.main.description;
+    const iconId = weatherData.weather[0].icon;
+    const iconName = weatherData.weather[0].description;
 
     return {temperature, wind, humidity, pressure, iconId, iconName};
 };
 
 const weatherQueriesWithPlaceName = async(placeName) => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${place.city},${place.state},${place.country}&appid=${}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${place.city},${place.state},${place.country}&units=${linkMetaData.unit}&appid=${linkMetaData.openWeatherKey}`;
     const response = await fetch(URL, {
         "Method": "GET"
     });
